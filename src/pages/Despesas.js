a9a3addaf5c53f146769d9e43429e1f5230b5beb
Felipe <felipe.seraf@gmail.comgit config --global user.name Felipegit coomit -m Firstgit config --global user.email felipe.seraf@gmail.com>
2019-11-25 15:11:55 -0300

    opa

--- src/pages/Despesas.js
+++ src/pages/Despesas.js
@@ -10,13 +10,13 @@ class Despesas extends Component {
 
 
     async componentDidMount() {
-        this.setState({ loading: false });
+        this.setState({ loading: true });
         const id = this.props.location.state.id;
         const url = `https://cors-anywhere.herokuapp.com/https://dadosabertos.camara.leg.br/api/v2/deputados/${id}/despesas?ordem=ASC&ordenarPor=ano`;
 
         const response = await axios.get(url)
         console.log(response.data)
-        this.setState({ deps: response.data.dados, loading: false });
+        this.setState({ despesas: response.data.dados, loading: false });
 
     }
 
@@ -25,21 +25,23 @@ class Despesas extends Component {
 
             <Container>
                 {this.state.loading ?
-                    <Spinner animation="border" role="status">
-                        <span className="sr-only">Loading...</span>
-                    </Spinner>
-                    : null
-
+                    <div className="d-flex justify-content-center">
+                        <Spinner animation="border" role="status">
+                            <span className="sr-only">Loading...</span>
+                        </Spinner>
+                    </div>
+                    :
+                    <div>
+                        <h1>Despesas</h1>
+                        {this.state.despesas.map(despesa => (
+                            <Card>
+                                <Card.Body>
+                                    {JSON.stringify(despesa)}
+                                </Card.Body>
+                            </Card>
+                        ))}
+                    </div>
                 }
-
-                <h1>Despesas</h1>
-                {this.state.despesas.map(despesa => (
-                    <Card>
-                        <Card.Body>
-                            {JSON.stringify(despesa)}
-                        </Card.Body>
-                    </Card>
-                ))}
             </Container>
         );
     }
--- src/pages/Index.js
+++ src/pages/Index.js
@@ -17,34 +17,38 @@ class Index extends Component {
     }
 
     handleClick = (id) => {
-        this.props.history.push('/Despesas', {id: id})
+        this.props.history.push('/Despesas', { id: id })
     }
 
     render() {
         return (
-            <Container>
+            <Container >
                 {this.state.loading ?
-                    <Spinner animation="border" role="status">
-                        <span className="sr-only">Loading...</span>
-                    </Spinner>
-                    : null
-
+                    <div className="d-flex justify-content-center">
+                        <Spinner animation="border" role="status" >
+                            <span className="sr-only">Loading...</span>
+                        </Spinner>
+                    </div>
+                    :
+                    <div>
+                        <h1>Deputados</h1>
+                        {this.state.deps.map(dep => (
+                            <Card key={dep.id} onClick={() => this.handleClick(dep.id)}>
+                                <Card.Body >
+                                    {/* <img src={article.urlToImage} /> */}
+                                    <h1>{dep.nome}</h1>
+                                    <h1>{dep.id}</h1>
+                                    {/* <p>{article.content}</p>
+                                    <a href={article.url} target="_blank">Link</a> */}
+                                </Card.Body>
+                            </Card>
+                        ))}
+                    </div>
                 }
-                <h1>Deputados</h1>
-                {this.state.deps.map(dep => (
-                    <Card key={dep.id} onClick={() => this.handleClick(dep.id)}>
-                        <Card.Body >
-                                {/* <img src={article.urlToImage} /> */}
-                                <h1>{dep.nome}</h1>
-                                <h1>{dep.id}</h1>
-                                {/* <p>{article.content}</p>
-                                <a href={article.url} target="_blank">Link</a> */}
-                        </Card.Body>
-                    </Card>
-                ))} 
+
 
             </Container>
-           
+
         );
     }
 }
