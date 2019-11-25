b1bff99cfe77242c921cd070db5511548cb81791
Felipe <felipe.seraf@gmail.comgit config --global user.name Felipegit coomit -m Firstgit config --global user.email felipe.seraf@gmail.com>
2019-11-25 10:59:13 -0300

    added spinner

--- src/pages/Despesas.js
+++ src/pages/Despesas.js
@@ -1,25 +1,37 @@
 import React, { Component } from 'react';
-import {Container, Card} from 'react-bootstrap'
+import { Container, Card, Spinner } from 'react-bootstrap'
 import axios from 'axios'
 
 class Despesas extends Component {
     state = {
-        despesas: []
+        despesas: [],
+        loading: false
     }
 
 
     async componentDidMount() {
+        this.setState({ loading: false });
         const id = this.props.location.state.id;
         const url = `https://cors-anywhere.herokuapp.com/https://dadosabertos.camara.leg.br/api/v2/deputados/${id}/despesas?ordem=ASC&ordenarPor=ano`;
 
         const response = await axios.get(url)
-console.log(response.data)
-        this.setState({ despesas: response.data.dados });
+        console.log(response.data)
+        this.setState({ deps: response.data.dados, loading: false });
+
     }
 
     render() {
         return (
+
             <Container>
+                {this.state.loading ?
+                    <Spinner animation="border" role="status">
+                        <span className="sr-only">Loading...</span>
+                    </Spinner>
+                    : null
+
+                }
+
                 <h1>Despesas</h1>
                 {this.state.despesas.map(despesa => (
                     <Card>
--- src/pages/Index.js
+++ src/pages/Index.js
@@ -1,17 +1,19 @@
 import React, { Component } from 'react';
-import { Container, Card } from 'react-bootstrap'
+import { Container, Card, Spinner } from 'react-bootstrap'
 import axios from 'axios'
 class Index extends Component {
     state = {
-        deps: []
+        deps: [],
+        loading: false
     }
 
     async componentDidMount() {
+        this.setState({ loading: true });
         const url = `https://cors-anywhere.herokuapp.com/https://dadosabertos.camara.leg.br/api/v2/deputados?siglaUf=ES&ordem=ASC&ordenarPor=nome`;
 
         const response = await axios.get(url)
 
-        this.setState({ deps: response.data.dados });
+        this.setState({ deps: response.data.dados, loading: false });
     }
 
     handleClick = (id) => {
@@ -21,6 +23,13 @@ class Index extends Component {
     render() {
         return (
             <Container>
+                {this.state.loading ?
+                    <Spinner animation="border" role="status">
+                        <span className="sr-only">Loading...</span>
+                    </Spinner>
+                    : null
+
+                }
                 <h1>Deputados</h1>
                 {this.state.deps.map(dep => (
                     <Card key={dep.id} onClick={() => this.handleClick(dep.id)}>
